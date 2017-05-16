f1a37685f7fd2ba649331453509a631c6ef86384
phuongdo96 <phuongnt9121996@gmail.com>
2017-05-16 17:37:09 +0700

    Java

---
+++ Bai13_TH4_DoManhPhuong_56135039/gen/com/example/Bai13_TH4_DoManhPhuong_56135039/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bai13truonghop4nguyennhuy561317778;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ baiTap11SuDungCheckboxVaRadioButtonTrongAndroid/gen/com/example/baitap11sudungcheckboxvaradiobuttontrongandroidnguyennhuy_56131778/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.baitap11sudungcheckboxvaradiobuttontrongandroidnguyennhuy_56131778;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
