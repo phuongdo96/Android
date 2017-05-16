4a51089f603aac36d8d4b1c3511ad143e5673167
phuongdo96 <phuongnt9121996@gmail.com>
2017-05-16 17:37:30 +0700

    Java

---
+++ BaiTap12ViDuTongHopTextViewEditTextCheckBoxButtonVaImageButtonTrongAndroid/gen/com/example/Bai12_Java/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.NguyenNhuY_56th2_56131778_Bai12_Java;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
