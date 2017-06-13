41f732b6a3fef755767274298fc29c2b109a5be1
phuongdo96 <phuongnt9121996@gmail.com>
2017-06-13 23:02:12 +0700

    Java
    Java

---
+++ LapTrinhAndroidCoBanBai30ListViewNangCao-CustomAdapter-Video_KhoaPham_NhuY_56th2/gen/com/example/DoManhPhuong/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.DoManhPhuong;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LapTrinhAndroidCoBanBai30ListViewNangCao-CustomAdapter-Video_KhoaPham_NhuY_56th2/src/com/example/DoManhPhuong/SinhVien.java
@@ -0,0 +1,13 @@
+package com.example.DoManhPhuong;
+
+
+public class SinhVien {
+	public String HoTen;
+	public Integer NamSinh;
+	
+	public SinhVien(String hoTen, Integer namSinh ){
+		HoTen = hoTen;
+		NamSinh = namSinh;
+	}
+
+}
